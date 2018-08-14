809e958e285aeb8c91dc4528cb77b9fdd28901c2
Karina Ionkina <kionkina@stuy.edu>
2018-08-13 20:05:50 -0400

    cpp

---
+++ cpp/hello.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#Fib  
+int nthfib(int n){
+    if (n == 0){
+      return 0;
+    }
+    if (n == 1){
+      return 1;
+    }
+    else {
+      return nthfib(n-1) + nthfib(n-2);
+    }
+  }
+
+
+int main()
+{
+  std::string answer;
+
+  while (answer != "no"){
+  int input;
+  std::cout << "Running nthfib()... \n";
+  std::cout << "Enter n: ";
+  std::cin >> input;
+  int d = nthfib(input);
+  std::cout << "\n The " << input << "th term is: "<< d << "\nAnother entry? (yes or no) ";
+  std::cin >> answer;
+  }
+  
+
+
+}
